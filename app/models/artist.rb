c906a8d53e458385c11518d12a53fa0164da5191
pahoffart25 <pahoffart25@gmail.com>
2020-02-01 14:23:52 -0600

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,43 @@
 class Artist
 
+  @@all = []
+
   attr_reader :name, :years_experience
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+
+    @@all << self    
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select{|painting| painting.artist == self}
+    
+  end
+
+  def galleries
+    paintings.map{|painting| painting.gallery}
+  end
+
+  def cities
+    galleries.map{|gallery| gallery.city}.uniq
+  end
+
+  def self.total_experience
+    @@all.inject(0){|total, artist| total + artist.years_experience}
+  end
+
+  def self.most_prolific
+    @@all.max_by {|artist| artist.paintings.length / artist.years_experience}
+  end
+
+  def create_painting(title,price,gallery)
+    Painting.new(title, price, self, gallery) 
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,8 +1,34 @@
 require_relative '../config/environment.rb'
+#require 'pry'
+
+a1 = Artist.new("Van Gogh", 12)
+a2 = Artist.new("Munch", 17)
+a3 = Artist.new("Da Vinci", 15)
+a4 = Artist.new("Wood", 8)
+a5 = Artist.new("Goya", 10)
+
+g1 = Gallery.new("M.o.m.a", "New York")
+g2 = Gallery.new("National Gallery", "Oslo")
+g3 = Gallery.new("Louvre", "Paris")
+g4 = Gallery.new("Art Institute", "Chicago")
+g5 = Gallery.new("Museo Lazaro Galdiano", "Madrid")
 
 
+
+p1 = Painting.new("Starry Night", 100, a1, g1)
+p2 = Painting.new("The Scream", 200, a2, g2)
+p3 = Painting.new("Mona Lisa", 300, a3, g3)
+p4 = Painting.new("American Gothic", 400, a4, g4)
+p5 = Painting.new("Witches Sabbath", 500, a5, g5)
+p6 = Painting.new("Self Potrait", 600, a1, g5)
+p7 = Painting.new("The Sick Child", 700, a2, g4)
+p8 = Painting.new("The Last Supper", 800, a3, g3)
+p9 = Painting.new("Young Corn", 900, a4, g2)
+p10 = Painting.new("Los Disparates", 1000, a5, g1)
+
+a1.create_painting("new one", 300, g1)
 
 
 binding.pry
 
-puts "Bob Ross rules."
+puts "Bob Ross is mediocre."
