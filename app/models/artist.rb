4e0594304fcf39fe11b71b70ce02001fb735a133
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-02-02 18:30:04 -0600

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,64 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def paintings
+    Painting.all.select { |painting|
+      painting.artist == self
+    }
+  end
+
+  def galleries
+    paintings.map { |painting|
+      painting.gallery
+    }.uniq
+  end
+
+  def cities
+    galleries.map{ |gallery|
+      gallery.city
+    }.uniq
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, self, price, gallery)
+  end
+
+
+
+  def self.all
+    @@all
+  end
+
+  def self.total_experience
+    @@all.sum{ |sum = 0, artist|
+      sum = sum + artist.years_experience
+    }
+  end
+
+  def self.most_prolific
+    @@all.max_by{ |artist|
+      artist.paintings.length / artist.years_experience
+    }
   end
 
 end
+
+
+
+#   * `Artist.total_experience`
+#     * Returns an `integer` that is the total years of experience of all artists
+
+#   * `Artist.most_prolific`
+#     * Returns an `instance` of the artist with the highest amount of paintings per year of experience.
+
+#   * `Artist#create_painting`
+#     * Given the arguments of `title`, `price` and `gallery`, creates a new painting belonging to that artist
+
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,60 @@
 class Gallery
 
   attr_reader :name, :city
+  
+  
+  @@all = []
 
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def paintings
+    Painting.all.select { |painting|
+      painting.gallery == self
+    }
+  end
+
+  def artists
+    paintings.map {|painting|
+      painting.artist
+    }.uniq
+  end
+
+  def artist_names
+    artists.map { |artist|
+      artist.name
+    }
+  end
+
+  def most_expensive_painting
+    paintings.max_by{ |painting|
+      painting.price
+    }
+  end
+
+  def self.all
+    @@all
   end
 
 end
+
+
+# **GALLERY**
+
+#   * `Gallery.all`
+#     * Returns an `array` of all the galleries
+
+#   * `Gallery#paintings`
+#     * Returns an `array` of all paintings in a gallery
+
+#   * `Gallery#artists`
+#     * Returns an `array` of all artists that have a painting in a gallery
+
+#   * `Gallery#artist_names`
+#     * Returns an `array` of the names of all artists that have a painting in a gallery
+
+#   * `Gallery#most_expensive_painting`
+#     * Returns an `instance` of the most expensive painting in a gallery
