698c2e96395714004f1731c34d4b2335486408c7
placidowang <placidow@gmail.com>
2020-02-04 11:04:16 -0600

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,39 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map {|painting| painting.gallery}
+  end
+
+  def cities
+    galleries.map {|gallery| gallery.city}.uniq
+  end
+
+  def self.total_experience
+    self.all.reduce(0) {|sum, artist| sum + artist.years_experience}
+  end
+
+  def self.most_prolific
+    self.all.max {|a, b| a.paintings.count/a.years_experience <=> b.paintings.count/b.years_experience}
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,14 @@
 require_relative '../config/environment.rb'
 
+a1 = Artist.new("Bob Ross", 25)
+a2 = Artist.new("Elmo", 1)
 
+g1 = Gallery.new("Le Louvre", "Paris")
+g2 = Gallery.new("MOMA", "Houston")
+
+p1 = Painting.new("My Backyard", 150, a1, g2)
+p2 = Painting.new("Colorado Winter", 280, a1, g1)
+p3 = Painting.new("Dorothy", 5, a2, g2)
 
 
 binding.pry
