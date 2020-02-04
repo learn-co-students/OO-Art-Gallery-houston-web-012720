d6b5c13c1661874e56380b3f8e922904f0c7bdcd
timothyalton <mitchell.alton@live.com>
2020-02-04 11:16:17 -0600

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,43 @@
 class Artist
 
   attr_reader :name, :years_experience
-
+  @@all = []
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+
+    @@all << self
   end
 
+  def self.all 
+    @@all 
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map {|painting| painting.gallery.name}
+  end
+
+  def cities
+    paintings.map {|painting| painting.gallery.city}
+  end
+
+
+  def self.total_experience
+    # @@all.map {|years_experience| name.years_experience}.sum
+    @@all.reduce(0) {|sum, y| sum + y.years_experience}
+  end
+
+  def self.most_prolific
+    @@all.max {|artist| (artist.paintings.count / artist.years_experience)}
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
+  
 end
--- tools/console.rb
+++ tools/console.rb
@@ -2,6 +2,27 @@ require_relative '../config/environment.rb'
 
 
 
+g1 = Gallery.new("Houston Art Gallery", "Houston")
+g2 = Gallery.new("San Jose Art Gallery", "San Jose")
+g3 = Gallery.new("Dallas Art Gallery", "Dallas")
+g4 = Gallery.new("Central Park Art Gallery", "New York")
+g5 = Gallery.new("Sacramento Art Gallery", "Sacramento")
+
+a1 = Artist.new("Juan", 2)
+a2 = Artist.new("Michael", 15)
+a3 = Artist.new("Issac", 24)
+a4 = Artist.new("Jessica", 5)
+
+p1 = Painting.new("Mona Lisa", 100000000, a1, g1)
+p2 = Painting.new("Fake Mona Lisa", 100, a3, g5)
+p3 = Painting.new("Michaelangelo's First Painting", 150000, a3, g5)
+p4 = Painting.new("The River", 1500, a4, g2)
+p5 = Painting.new("Amatuer", 150, a2, g4)
+p6 = Painting.new("Dos Attempt", 1500, a2, g4)
+p7 = Painting.new("Tranquil", 100, a2, g4)
+p8 = Painting.new("Serendipity", 120, a2, g3)
+
+
 
 binding.pry
 
