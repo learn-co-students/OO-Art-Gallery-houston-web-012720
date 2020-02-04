20fd0d51ac6bb1d193f9688ca0775bb140d1f09e
corn-cloud <comowaiye@gmail.com>
2020-02-04 17:41:16 -0600

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,65 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
+  
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self 
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|p| p.artist == self}
+  end 
+
+  def galleries
+    paintings.map {|g| g.gallery} 
+  end 
+
+  def cities
+    paintings.map {|g| g.gallery.city}
+  end 
+  def self.total_experience
+    self.all.sum {|a| a.years_experience}
+  end
+
+  # def self.most_prolific
+    # @@all.map do|a| 
+    #   year_of_exp = a.years_experience
+    #   num_of_pantings = Painting.all.select {|p| p.artist == a}.count
+    #   prolific = num_of_pantings / years_of_exp 
+    # end
+
+    
+
+
+    
+    
+  # end
+
+  def self.most_prolific
+    a = @@all[0]
+
+    @@all.each do |artist|
+      if (artist.paintings.count/artist.years_experience) > (a.paintings/a.years_experience)
+        a = artist
+      end
+    end
+
+    a
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end 
+
 end
+
+
+
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,32 @@
 class Gallery
 
   attr_reader :name, :city
+  @@all = []
 
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self 
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def paintings
+    Painting.all.select {|p| p.gallery == self}
+  end 
+
+  def artists
+    paintings.map {|a| a.artist}
+  end
+
+  def artist_names
+    paintings.map {|a| a.artist.name}
+  end
+
+  def most_expensive_painting
+    paintings.max_by {|p| p.price}
   end
 
 end
