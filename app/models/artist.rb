4983d49889805772daf42f6871f51591154f5cb4
SteRobWms <stephen.r.williams.87@gmail.com>
2020-02-05 08:46:17 -0600

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,58 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select{|painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map{|painting| painting.gallery}.uniq
+  end
+
+  def cities
+    galleries.map{|gallery| gallery.city}.uniq
+  end
+
+  def self.total_experience
+    all.sum{|artist| artist.years_experience}
+  end
+
+  def self.most_prolific
+    all.max_by{|artist| artist.paintings.count.to_f / artist.years_experience}
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
+    # def self.artists_outputs
+  #   all.map{|artist| artist.paintings.count}
+  # end
+
+  # def self.artists_years_experience
+  #   all.map{|artist| artist.years_experience}
+  # end
+
+  # def self.most_prolific_ewww
+  #   outputs_per_year = artists_outputs.map.with_index do |output, index|  output.to_f/artists_years_experience[index]
+  #   end
+  #   max_output_per_year = outputs_per_year.max
+  #   all[outputs_per_year.find_index(max_output_per_year)]
+  # end
+
+  # def self.most_prolific_good
+  #   all.max{|a,b| a.paintings.length/ a.years_experience.to_f <=> b.paintings.length / b.years_experience.to_f}
+  # end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,68 @@
 require_relative '../config/environment.rb'
 
+# artist_names = %w(Vien Paul Blake Stephen Giorgii)
 
+# def create_artists(array)
+#     array.each.with_index do |artist, index|
+#         "a#{index}" = Artist.new(artist, rand(10))
+#     end
+# end
+# 5.times do
+#     Artist.new(artist_names.sample, rand(10))
+# end
+# # def create_artist(array)
+#     newarr = array.map.with_index do |artist, index|
+#         "a"+(index.to_s)
+#     end
+#     newerarr = newarr.map.with_index do |var, index|
+#         var = array[index]
+#     end
+#     p newerarr
+# end
+
+# create_artist(artist_names)
+
+a1 = Artist.new("Vien", 4)
+a2 = Artist.new("Paul", 7)
+a3 = Artist.new("Blake", 3)
+a4 = Artist.new("Stephen", 10)
+# art_array = [a1, a2, a3, a4]
+
+# gallery_list = %w(Archway Insomnia Creative_Bean, Dancing_Bear, Snifters, Lamplight)
+
+# city_list = %w(Houston, Waco, FtSmith)
+
+# def create_galleries(galleries, cities)
+#     galleries.each do |gallery|
+#         gallery = Gallery.new(gallery, cities.sample)
+#     end
+# end
+
+g1 = Gallery.new("Archway Gallery", "Houston")
+g2 = Gallery.new("Insomnia Gallery", "Houston")
+g3 = Gallery.new("Creative Bean", "Fort Smith")
+g4 = Gallery.new("Dancing Bear", "Waco")
+
+# gal_array = [g1, g2, g3, g4]
+
+painting_list = ["Red Blue Chair", "Luncheon on the Island of the Grande Jatte", "Guernica", "Arnolfini Portrait", "Scream", "Kiss", "Starry Night", "Persistence of Memory", "Treachery of Images", "Birth of Venus", "Self Portrait", "Venus of Urbino"]
+
+p1 = Painting.new("Red Blue Chair", 1000, a1, g1)
+p2 = Painting.new("Luncheon on the Island of the Grande Jatte", 500, a1, g2)
+p3 = Painting.new("Guernica", 2000, a2, g2)
+p4 = Painting.new("Arnolfini Portrait", 333, a2, g3)
+p5 = Painting.new("The Scream", 444, a2, g3)
+p6 = Painting.new("The Kiss", 589, a3, g4)
+p7 = Painting.new("Starry Night", 9898, a3, g1)
+p8 = Painting.new("Persistence of Memory", 742, a4, g3)
+p9 = Painting.new("Treachery of Images", 19192, a4, g4)
+p10 = Painting.new("Birth of Venus", 9919, a4, g2)
+
+# def create_paintings(array)
+#     array.each do |painting|
+#         painting.index = Painting.new(painting, rand(500), art_array.sample, gal_array.sample)
+#     end
+# end
 
 
 binding.pry
